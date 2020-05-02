df9bebea854dea3e0b3e21663e8cfc2528d51986
unknown <cozmalaura23@gmail.com>
2020-05-03 01:59:10 +0300

    Made a functional menu with sign out, cart and orders option

--- app/src/main/java/com/example/ready2eat/Home.java
+++ app/src/main/java/com/example/ready2eat/Home.java
@@ -2,6 +2,8 @@ package com.example.ready2eat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
@@ -20,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -30,7 +34,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration mAppBarConfiguration;
     FirebaseDatabase database;
@@ -59,8 +63,8 @@ public class Home extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              Intent cartItent = new Intent(Home.this, Cart.class);
+              startActivity(cartItent);
             }
         });
 
@@ -69,13 +73,20 @@ public class Home extends AppCompatActivity {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
+        /*mAppBarConfiguration = new AppBarConfiguration.Builder(    - am inlocuit cu ce e mai jos ca sa functioneze - Laura
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);*/
+
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         //Set name for user
         final View headerView = navigationView.getHeaderView(0);
@@ -115,6 +126,18 @@ public class Home extends AppCompatActivity {
             recyclerMenu.setAdapter(adapter);
         }
 
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -123,9 +146,41 @@ public class Home extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+
+        if(id == R.id.nav_menu) {
+            //handle the camera action
+        }
+        else if(id == R.id.nav_cart) {
+            Intent cartIntent = new Intent(Home.this, Cart.class);
+            startActivity(cartIntent);
+        }
+        else if(id == R.id.nav_orders) {
+            Intent orderIntent = new Intent(Home.this, OrderStatus.class);
+            startActivity(orderIntent);
+        }
+        else if(id == R.id.nav_log_out) {
+            Intent signIn = new Intent(Home.this, SignIn.class);
+            signIn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(signIn);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/ready2eat/ViewHolder/OrderViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.ready2eat.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ready2eat.Interface.ItemClickListener;
+import com.example.ready2eat.R;
+
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtOrderId;
+    public TextView txtOrderStatus;
+    public TextView txtOrderPhone;
+    public TextView txtOrderAddress;
+    private ItemClickListener itemClickListener;
+
+    public OrderViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtOrderAddress = (TextView)itemView.findViewById(R.id.order_address);
+        txtOrderId = (TextView)itemView.findViewById(R.id.order_id);
+        txtOrderStatus = (TextView)itemView.findViewById(R.id.order_status);
+        txtOrderPhone = (TextView)itemView.findViewById(R.id.order_phone);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        itemClickListener.onClick(v, getAdapterPosition(), false);
+    }
+}
