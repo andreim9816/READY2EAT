6d53bb518be2a315fc89159431b54d6f86f51ba1
alexandradonici <48128836+alexandradonici@users.noreply.github.com>
2020-05-09 17:28:36 +0300

    Add a new item
    Add a new item

--- app/src/main/java/com/example/ready2eat/Common/Common.java
+++ app/src/main/java/com/example/ready2eat/Common/Common.java
@@ -6,4 +6,8 @@ public class Common
 {
     public static User currentUser; // variables that saves the current user
 
+    public static final String UPDATE = "Actualizeaza";
+    public static final String DELETE = "Sterge";
+    public static final int PICK_IMAGE_REQUEST = 71;
+
 }
