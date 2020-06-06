5ab3b8217e3d263824114e4fe214a82aa85de44a
alexandradonici <48128836+alexandradonici@users.noreply.github.com>
2020-06-06 15:33:29 +0300

    Design page About us
    Design page About us

--- app/src/main/java/com/example/ready2eat/Common/Common.java
+++ app/src/main/java/com/example/ready2eat/Common/Common.java
@@ -15,19 +15,6 @@ public class Common
     public static final String PWD_KEY = "Password";
     public static final int PICK_IMAGE_REQUEST = 71;
 
-    public static String convertCodeToStatus(String status)
-    {
-        if(Objects.equals(status, new String("0")))
-            return "Plasata";
-        else if(Objects.equals(status, new String("1")))
-            return "Se poate prelua";
-        else
-            return "Preluata";
-    }
-
-    public static final String UPDATE = "Actualizeaza";
-    public static final String DELETE = "Sterge";
-    public static final int PICK_IMAGE_REQUEST = 71;
 
     public static String convertCodeToStatus(String status)
     {
